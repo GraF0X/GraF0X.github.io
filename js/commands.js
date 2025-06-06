8cf5eb4cba8aa942694ca35446dfbbda08c870ff
0x519 <76882227+GraF0X@users.noreply.github.com>
2025-06-06 17:11:16 +0300

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,76 @@
+var dev = "https://fzerox.xyz";
+var github = "https://github.com/null";
+var sudo = "https://www.youtube.com/watch?v=dQw4w9WgXcQ?autoplay=1";
+var email = "mailto:more.zgraf@gmail.com";
+var xero = "https://https://x-e.ro/";
+
+let banner = [
+  `<div id="banner-section" class="banner">
+ Welcome to my portfolio! — Type <span class="command">help</span> for a list of supported commands.
+  </div>`,
+];
+
+let aboutme = [
+  "<br>",
+  `<div id="aboutme-section">`,
+  `<span class='underline'>Hey, I'm Viacheslav! 👋</span>`,
+  "<br>",
+  `<li>📂 Use the <span class="command">projects</span> command to check out what I’ve built!</li>`,
+  `<li>📫 Contact: <a href="mailto:more.zgraf@gmail.com">more.zgraf@gmail.com</a></li>`,
+  "</div>",
+  "<br>",
+];
+
+let social = [
+  "<br>",
+  'dev        <a href="' + dev + '" target="_blank">fzerox.xyz</a>',
+
+  'github         <a href="' +    github +    '" target="_blank">github/null</a>',
+  "<br>",
+];
+
+let friend = [
+  "<br>",
+  'Cool Dude        <a href="' + xero + '" target="_blank">Xer0 <img src="https://whois.x-e.ro/ui/img/metroid.gif"></a>',
+
+  
+  "<br>",
+];
+
+let help = [
+  `<br><div id="help-section"><pre class="whitespace-pre-wrap">
+<span class="command">aboutme</span>
+↳ Displays who I am?
+<span class="command">social</span>
+↳ Lists social networks.
+<span class="command">projects</span>
+↳ View coding projects.
+<span class="command">email</span>
+↳ To send me an email.
+<span class="command">history</span>
+↳ View command history.
+<span class="command">help</span>
+↳ Displays this help message.
+<span class="command">sudo</span>
+↳ Try it out for yourself.
+<span class="command">snake</span>
+↳ Run Snake Game.
+<span class="command">clear</span>
+↳ Clear the terminal.
+</pre></div><br>`,
+];
+let projects = [
+  "<br>",
+  `<div id="projects-section">`,
+  `<a href="https://github.com/null/" target="_blank"><span class='underline'>Title</span></a>`,
+  `<pre class="indent-8 whitespace-pre-wrap break-words overflow-x-auto">
+discription
+Live: <a href="https://google.com/" target="_blank">google.com</a>
+Built With:
+<3
+</pre>`,
+
+ 
+  "</div>",
+  "<br>",
+];
